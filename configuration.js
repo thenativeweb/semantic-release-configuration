422d32ba917d3b0c3a1c9b88d00a067ea771a298
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-11-29 10:25:17 +0100

    feat: Setup project. (#1)
    * Setup project.
    
    * Fix ESLint configuration.
    
    * Update roboter.

---
+++ configuration.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const configuration = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ]
+};
+
+module.exports = configuration;
